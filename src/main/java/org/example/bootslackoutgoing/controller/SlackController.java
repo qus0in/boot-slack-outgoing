99ec3428137cb2b171d12e230853308313c41668
qus0in <qus0in@gmail.com>
2025-05-22 15:44:51 +0900

    푸시푸시

--- src/main/java/org/example/bootslackoutgoing/controller/SlackController.java
+++ src/main/java/org/example/bootslackoutgoing/controller/SlackController.java
@@ -1,6 +1,8 @@
 package org.example.bootslackoutgoing.controller;
 
+import org.example.bootslackoutgoing.dto.SlackAnswerDTO;
 import org.example.bootslackoutgoing.dto.SlackWebhookDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SlackController {
     @PostMapping
     // Form
-    public void post(@ModelAttribute SlackWebhookDTO dto) {
+    public ResponseEntity<SlackAnswerDTO> post(@ModelAttribute SlackWebhookDTO dto) {
+        if (dto.user_name().equals("slackbot")) {
+//            throw new RuntimeException("자문자답 금지");
+            return ResponseEntity.ok(new SlackAnswerDTO(""));
+        }
         System.out.println(dto);
+        return ResponseEntity.ok(new SlackAnswerDTO("%s에 대한 대답입니다".formatted(dto.text())));
     }
 }
